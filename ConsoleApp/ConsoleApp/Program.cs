278122e6af23e7f23faca8291807e5e6a6e98b71
svillamonte <walden2av@gmail.com>
2022-08-22 10:25:32 +1200

    Initial commit

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = new int[] { 0, 1, 2 };
+var result = array.ToList();
+
+Console.WriteLine(result);
